eed9303a4d71facb7bbf2dfadf5045bb9f681862
Alejandro Luces González <aluces@polytex.local>
2023-06-03 23:00:27 -0400

    Adding_exercise_jschallenger_V5

---
+++ Js_Fundamentals_V5.js
@@ -0,0 +1,17 @@
+// Write a function that takes a string (a) as argument. 
+// Remove the first 3 characters of a. Return the result
+
+// Escribe una función que tome una cadena (a) como argumento. 
+// Elimina los 3 primeros caracteres de a. Devolver el resultado
+
+function myFunction(a){
+
+    return a.substring(4)
+
+}
+
+
+console.log(myFunction('abcdefg'));
+console.log(myFunction('1234'));
+console.log(myFunction('fgedcba'));
+
