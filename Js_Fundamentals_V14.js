e344c919642b4e40e3ee3f4c49227185d335a134
Alejandro Luces González <aluces@polytex.local>
2023-06-05 18:15:09 -0400

    Adding_exercise_jschallenger_V14 modified

---
+++ Js_Fundamentals_V14.js
@@ -0,0 +1,19 @@
+// Write a function that takes a number as argument. 
+// If the number is even, return true. Otherwise, return false
+
+// Escribe una función que tome un número como argumento. 
+// Si el número es par, devuelve verdadero. De lo contrario, devuelve falso
+
+
+function myFunction(a) {
+   
+        return a%2===0
+
+    }
+    console.log(myFunction(10));
+    console.log(myFunction(-4));
+    console.log(myFunction(5));
+    console.log(myFunction(-111));
+    
+    
+    
