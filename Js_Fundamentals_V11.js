1f637f0a22fe9da24d65d2f9a414c93b7bc68ccb
Alejandro Luces González <aluces@polytex.local>
2023-06-05 14:31:19 -0400

    Adding_exercise_jschallenger_V11

---
+++ Js_Fundamentals_V11.js
@@ -0,0 +1,17 @@
+// Write a function that takes two numbers (a and b) as argument. 
+// Return b percent of a
+
+// Escribe una función que tome dos números (a y b) como argumento. 
+// Devolver b por ciento de a
+
+
+function myFunction(a,b) {
+
+    return b/100*a
+    
+    }
+    
+    console.log(myFunction(100,50));
+    console.log(myFunction(10,1));
+    console.log(myFunction(500,25));
+    
