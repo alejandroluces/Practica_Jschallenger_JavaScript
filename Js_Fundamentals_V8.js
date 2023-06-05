76b76414310b8a7e0b8eb4bd9acd3f3472836c26
Alejandro Luces González <aluces@polytex.local>
2023-06-04 21:35:16 -0400

    Adding_exercise_jschallenger_V8

---
+++ Js_Fundamentals_V8.js
@@ -0,0 +1,15 @@
+// Write a function that takes a string as argument. 
+// The string contains the substring 'is'. Return the index of 'is'.
+
+// Escribe una función que tome una cadena como argumento. 
+// La cadena contiene la subcadena 'es'. Devuelve el índice de 'es'.
+
+function myFunction(a) {
+
+    return a.substring("is")
+    
+    }
+    
+    console.log(myFunction("praise"));
+    console.log(myFunction("risky"));
+    console.log(myFunction("paris"));
