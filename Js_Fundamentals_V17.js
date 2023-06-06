9490659827c40bc6526c695d8025989d6a450ec6
Alejandro Luces González <aluces@polytex.local>
2023-06-06 17:45:18 -0400

    Adding_exercise_jschallenger_V17

---
+++ Js_Fundamentals_V17.js
@@ -0,0 +1,26 @@
+// Write a function that takes two numbers (a and b) as arguments. If a is smaller than b, divide a by b. 
+// Otherwise, multiply both numbers. 
+// Return the resulting value
+
+
+// Escribe una función que tome dos números (a y b) como argumentos. 
+// Si a es más pequeño que b, divide a por b. De lo contrario, multiplica ambos números. 
+// Devolver el valor resultante
+
+
+
+function myFunction(a,b){
+
+    return a<b ? a/b: a*b 
+
+}
+
+console.log(myFunction(10, 100));
+console.log(myFunction(90, 45));
+console.log(myFunction(8, 20));
+console.log(myFunction(2, 0.5));
+
+
+
+
+
